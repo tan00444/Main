7c5c36175530abe01b9bf8bfbe65fc1c02d879c9
Tan Huynh <104268347+tan00444@users.noreply.github.com>
2023-03-01 19:23:37 +0700

    update feedback

--- Main/Models/Feedback.cs
+++ Main/Models/Feedback.cs
@@ -1,6 +1,12 @@
-﻿namespace Main.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Main.Models
 {
     public class Feedback
     {
+        [Key]
+        public int feed_id { get; set; }
+        public int feed_content { get; set;}
+        public int feed_date { get; set;}
     }
 }
