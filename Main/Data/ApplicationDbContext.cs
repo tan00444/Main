9534a9434f08aca74b58deff2cfd0a111dc2ccd2
Tan Huynh <104268347+tan00444@users.noreply.github.com>
2023-03-01 18:50:17 +0700

    Add project files.

---
+++ Main/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Main.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Main.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Customer> customers { get; set; }
+        public DbSet<Category> categories { get; set; }
+        public DbSet<Product> products { get; set; }
+        public DbSet<Order> orders { get; set; }
+        public DbSet<OrderDetail> orderdetails { get; set; }
+        // khai bao la co bao nhieu cap khoa
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<OrderDetail>().HasKey(m => new { m.order_id, m.pro_id });
+        //}
+    }
+}
---
+++ Main/Models/Product.cs
@@ -0,0 +1,6 @@
+﻿namespace Main.Models
+{
+    public class Product
+    {
+    }
+}
